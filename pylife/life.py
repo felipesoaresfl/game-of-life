92644f18f6c3a9daacd0d76494d350d62f25db46
Felipe Soares <felipe.soares013@outlook.com>
2023-01-04 06:50:02 -0300

    inicia lógica

--- pylife/life.py
+++ pylife/life.py
@@ -7,3 +7,9 @@ COLOR_GRID = (110, 110, 110)
 COLOR_DYING = (174, 97, 250)
 COLOR_ALIVE = (72, 61, 139)
 
+def update(screen, cells, size, progress=False):
+    update_cells = numpy.empty((cells.shape[0], cells.shape[1]))
+
+    for row, col in numpy.ndindex(cells.shape):
+        alive = numpy.sum(cells[row-1:row+2, col-1:col+2]) - cells[row, col]
+        color = COLOR_BACKGROUND if cells[row, col] == 0 else COLOR_ALIVE
